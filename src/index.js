f62ecd8cf7d1e9d23d5ea5591643a6241847df8f
Dereje <getutadessedereje@icloud.com>
2022-05-22 18:42:02 +0200

    implementation de charte js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import '../styles/style.scss';
+const Chart = require('chart.js/auto').default;
+// const ctx = document.getElementsById('canvas').getContext('2d');
+const ctx = document.querySelector('canvas').getContext('2d');
+
+const days= ["mon", "tue", "wnd", "thu", "fri", "sat", "sun"];
