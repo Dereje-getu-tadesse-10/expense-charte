30a27635881bf4116a9c439f3d8ca99b4f8ac42e
Dereje <getutadessedereje@icloud.com>
2022-05-22 19:10:32 +0200

    ajout valeur json dans un array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import '../styles/style.scss';
 const Chart = require('chart.js/auto').default;
+const axios = require('axios')
+
 // const ctx = document.getElementsById('canvas').getContext('2d');
 const ctx = document.querySelector('canvas').getContext('2d');
+const url = './data.json';
 
-const days= ["mon", "tue", "wnd", "thu", "fri", "sat", "sun"];
+const arrDays = [];
+const arrAmount = [];
+
+const getValue = async () => {
+    try {
+      const response = await axios.get(url);
+
+      const res = response.data;
+
+    //   console.log(res[0].day);
+      res.forEach(element => {
+        // console.log(element.day);
+
+        arrDays.push(element.day);
+        arrAmount.push(element.amount);
+      });
+
+    } catch (errors) {
+      console.error(errors);
+    }
+};
+
+getValue()
