928671221ece5fdf46bb237814ec96041e14b596
Dereje <getutadessedereje@icloud.com>
2022-05-22 21:27:25 +0200

    amelioration du code charte js

--- src/index.js
+++ src/index.js
@@ -1,78 +1,93 @@
+import { Legend } from 'chart.js';
 import '../styles/style.scss';
 const Chart = require('chart.js/auto').default;
 const axios = require('axios')
 
 const ctx = document.querySelector('canvas').getContext('2d');
-const url = './data.json';
+const url = 'data.json';
 
-const arrDays = [];
-const arrAmount = [];
+const arrDays = [
+    'mon',
+    'tue',
+    'wed',
+    'thu',
+    'fri',
+    'sat',
+    'sun'
+];
+const arrAmount = [
+    '17.45',
+    '34.91',
+    '52.36',
+    '31.07',
+    '23.39',
+    '43.28',
+    '25.48'
+];
 
-const getValue = async () => {
-    try {
-      const response = await axios.get(url);
-      const res = response.data;
-      res.forEach(element => {
-        arrDays.push(element.day);
-        arrAmount.push(element.amount);
-      });
-    } catch (errors) {
-      console.error(errors);
-    }
-};
-getValue();
 
-function amountCharte() {
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: arrDays,
-            datasets: [{
-                data: arrAmount,
-                borderRadius:[5],
-                backgroundColor: [
-                    'hsl(10, 79%, 65%)',
-                    'hsl(10, 79%, 65%)',
-                    'hsl(186, 34%, 60%)',
-                    'hsl(10, 79%, 65%)',
-                    'hsl(10, 79%, 65%)',
-                    'hsl(10, 79%, 65%)',
-                    'hsl(10, 79%, 65%)'
-                ]
-            }]
+
+const data = {
+    labels: arrDays,
+    datasets: [{
+      data: arrAmount,
+      borderRadius:[5],
+      backgroundColor: [
+            'hsl(10, 79%, 65%)',
+            'hsl(10, 79%, 65%)',
+            'hsl(186, 34%, 60%)',
+            'hsl(10, 79%, 65%)',
+            'hsl(10, 79%, 65%)',
+            'hsl(10, 79%, 65%)',
+            'hsl(10, 79%, 65%)'
+      ]
+    }]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+        plugins:{
+            legend:{
+                display:false
+            },
+            title:{
+                display:false
+            },
+            datalabels: {
+                display: false
+            }
         },
-        options: {
-            plugins: {
-                  datalabels: {
-                    display: false
-                  }
-                },
-                yAxes: [{
-                    gridLines: {
-                      drawBorder: false,
-                    },
-                  }],
-            scales: {
-                x: {
-                  grid: {
-                    display: false,
+        yAxes: [{
+            gridLines: {
+                drawBorder: false,
+            },
+        }],
+        scales:{
+            x:{
+                grid:{
+                    display:false,
                     drawBorder:false,
                     drawOnChartArea: false,
-                  }
+                }
+            },
+            y:{
+                beginAtZero: true,
+                ticks:{
+                    display:false
                 },
-                y: {
-                    beginAtZero: true,
-                    ticks:{
-                        display:false
-                    },
-                  grid: {
+                grid:{
                     display: false,
                     drawBorder:false,
                     drawOnChartArea: true,
-                  },
-              }
+                }
             }
         }
-    })
-}
-amountCharte();
+    }
+  };
+
+   const amountChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
